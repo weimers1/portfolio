d0473ce578ff8529765da309651f366249603aa9
Sam Weimer <samweimer7@gmail.com>
2025-02-03 08:36:23 -0500

    feat: implement db connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-import express from "express";
-import { PORT } from "./config.js";
+import express from 'express';
+import { PORT, CONNECTIONSTRING } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.status(200).send('Hello World');
+});
+
 app.listen(PORT, () => {
-    console.log("App is listening on port " + PORT);
+    console.log('App is listening on port ' + PORT);
+});
+
+mongoose.connect(CONNECTIONSTRING).then((response) => {
+    console.log('Connected to database.');
+}).catch((error) => {
+    // @TODO: email errors
+    console.log(error);
 });
