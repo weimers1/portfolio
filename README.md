afd3086467d361f6cb8f213267998a6f8e9e1938
Sam Weimer <samweimer7@gmail.com>
2025-02-05 08:02:01 -0500

    feat: add client

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# portfolio
+# My Portfolio
+
+Welcome to my portfolio repo. Below you will find some information about the project.
+
+## Stack
+
+### MongoDB
+
+### Express + Node
+
+### React + Vite
+
+### Tailwind CSS
--- server/index.js
+++ server/index.js
@@ -2,10 +2,34 @@ import express from 'express';
 import { PORT, DB_CONNECTION_STRING } from './config.js';
 import mongoose from 'mongoose';
 import { Project, getGitHubRepoLanguages } from './models/project.js';
+import cors from 'cors';
 
+// Create server
 const app = express();
 
-app.get('/', (request, response) => {
+// Set server to listen on specified port
+app.listen(PORT, () => {
+    console.log('App is listening on port ' + PORT);
+});
+
+// Configure server middleware
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+app.get('/', async (request, response) => {
+    const projects = await Project.find({});
+    projects.forEach(async (project) => {
+        const projectLanguages = await getGitHubRepoLanguages(
+            project['titleRepo']
+        );
+        console.log(projectLanguages);
+    });
+
     return response.status(200).send('Hello World');
 });
 
@@ -18,16 +42,13 @@ app.get('/projects', async (request, response) => {
             );
             console.log(projectLanguages);
         });
+
         return response.status(200).json(projects);
     } catch (error) {
         // @TODO: email errors
         console.log(error);
         response.status(500).send('System Error');
     }
-});
-
-app.listen(PORT, () => {
-    console.log('App is listening on port ' + PORT);
 });
 
 mongoose
