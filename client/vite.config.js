8f12402a27f491041d7fd573f380b2063358c7d6
Sam Weimer <samweimer7@gmail.com>
2025-02-05 08:06:05 -0500

    feat: config port

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { PORT } from './config';
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+        port: PORT,
+    },
     plugins: [react(), tailwindcss()],
 });
