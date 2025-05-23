fe58b61c26b23b4210c1ccfb25e9f942cd23c3d0
Sam Weimer <samweimer7@gmail.com>
2025-05-22 21:29:49 -0400

    fix: update port number in vite config to satisfy Docker issues with .gitignore

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { PORT } from './config';
+const PORT = process.env.VITE_PORT || 3000;
 
 // https://vite.dev/config/
 export default defineConfig({
